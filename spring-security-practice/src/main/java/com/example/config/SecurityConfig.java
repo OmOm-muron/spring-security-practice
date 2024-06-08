ad9219cb02227a8da5e56a1037e35a8bf5e8a9e8
OmOmrone <43139003+OmOm-muron@users.noreply.github.com>
2024-06-09 00:21:34 +0900

    authentication by xml-defined information

--- spring-security-practice/src/main/java/com/example/config/SecurityConfig.java
+++ spring-security-practice/src/main/java/com/example/config/SecurityConfig.java
@@ -1,30 +1,53 @@
 package com.example.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Value("${app.user-xml-file}")
+    private String userXMLFileName;
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authz -> authz
                 .anyRequest().authenticated()
         ).formLogin(login -> login
                 .loginProcessingUrl("/login")
-                // If you define 'loginPage', default pages are disabled. ex) /login, /login?error, /login?logout
-                //.loginPage("/login")
                 .failureUrl("/login?error")
         ).logout(logout -> logout
-                // Set logout url accessed by GET or POST method, but GET page is defined 'logout' by default
-                // So if you set this other than 'logout', when send GET to the url, error will occur
                 .logoutUrl("/logout")
         );
         return http.build();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); 
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+        try {
+            List<UserDetails> users = UserXMLConfigurer.getUsers(userXMLFileName);
+            return new InMemoryUserDetailsManager(users);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
